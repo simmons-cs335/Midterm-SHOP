2d2fc00895cce202dca77488e28c9a60a9025c57
Clara Elizabeth Carleton <carletoc@simmons.edu>
2020-02-18 15:34:14 -0500

    main test functionality

--- src/ShoppingCartMain.java
+++ src/ShoppingCartMain.java
@@ -1,6 +1,7 @@
 public class ShoppingCartMain {
     public static void main(String[] args) {
 
+        /*
         UserDao userdao = new UserDao("carletoc", "1683864");
         userdao.list(); //Print out users
         userdao.delete(2); //Delete a user
@@ -9,6 +10,7 @@ public class ShoppingCartMain {
                 {"2","Clara","300 The Fenway","Boston", "MA", "02115", "clara@gmail.com"};
         userdao.save(columns); //Delete a user
         userdao.list(); //Print out users
+         */
 
         //Print out inventory
         /*
