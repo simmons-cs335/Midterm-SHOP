4ed9e76e7211d5019b622156f2612e334c5d7b50
crumcake <49697371+crumcake@users.noreply.github.com>
2020-02-27 17:46:29 -0500

    Update ShoppingCartMain.java

--- src/ShoppingCartMain.java
+++ src/ShoppingCartMain.java
@@ -9,4 +9,16 @@ public class ShoppingCartMain {
         cart.add(id, 10);
         System.out.println(cart.getItems(id));
     }
+    
+    public float calcTotal(Cart cart){
+        float total =0;
+        Hashtable<String, Double> cartItems = cart.getItems();
+        
+        /*Should pull out each item from the given cart.
+          just needs a getPrice() method in Cart*/
+        for(String key: cartItems.keySet()){
+            total += cartItems.get(key).getPrice();
+        }
+        return total;
+    }
 }
