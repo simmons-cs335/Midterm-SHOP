1cdd6f806dcb5349c996ae5b5e90f0752043c8ea
pragyeenepal <37906248+pragyeenepal@users.noreply.github.com>
2020-03-11 21:58:19 -0400

    Timer remove

--- src/ShoppingCartMain.java
+++ src/ShoppingCartMain.java
@@ -8,10 +8,7 @@ import java.util.TimerTask;
 public class ShoppingCartMain {
     public static void main(String[] args) {
 
-        runProgram t1 = new runProgram();
-        runProgram t2 = new runProgram();
-        Timer t = new Timer();
-        t.schedule(t1, 1000);
+    
 
 
 
