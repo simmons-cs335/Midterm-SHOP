6551deda9fb7b7569a8fdc470ad8941bd3810aba
Amran2 <hassana@simmons.edu>
2020-03-26 10:26:17 -0400

    Tried to Rewrite print Statements to make it more readable

--- src/MultithreadingProducer.java
+++ src/MultithreadingProducer.java
@@ -13,42 +13,35 @@ public class MultithreadingProducer extends Thread {
     }
     public void run() {
         Inventory product = new Inventory(prod_id);
+        System.out.println( "*****There are currently " + product.getNumberOfProducts() + " items listed in Inventory *****");
         try {
-            System.out.println("***** PRODUCER has STARTED looking at " + product.getProductName()+" in Inventory*****");
+            System.out.println("*****PRODUCER has STARTED looking at " + product.getProductName()+" in Inventory*****");
 
-            System.out.println( "There are currently " + product.getNumberOfProducts() + " items listed in Inventory");
+            System.out.printf("Producer is currently looking at: %s \n" +
+                            "\tIt belongs to the %s " + " department. \n" +
+                            "\tIt's current selling price is at $%s . \n" +
+                            "\tSo far, you have sold %s" + " units of this item. \n" +
+                            "\tAnd there are currently %s" + " left in stock \n",
+                            product.getProductName(), product.getProductDepartment(),
+                            product.getProductPrice(),  product.getSoldUnits(), product.getStockQuantity());
 
-            System.out.println("Producer is currently looking at "+ product.getProductName() );
-            System.out.println( product.getProductName()+ "  is located in the " + product.getProductDepartment() + " Department");
-            System.out.println("It's current selling price is at $ " + product.getProductPrice() );
-            //System.out.println("The Initial Stock was: " + product.getInitialQuantity() );
-            System.out.println( "The Amount of units Sold is: " + product.getSoldUnits() );
-            System.out.println( "Current Stock Left is: " + product.getStockQuantity() );
-
-            //restocking if there
-            int max = product.getNumberOfProducts();
-            int min = 1;
-            int random_item = (int)(Math.random()*(max-min+1)+min);
+            //addStock if there isn't enough stock
             int currentStockQuantity = product.getStockQuantity();
             Random rand = new Random();
-
-            int idealStockQuantity = rand.nextInt(50); //Random Number to determine the producer's idealStockQuant
+            int idealStockQuantity = rand.nextInt(100); //Random Number to determine the producer's idealStockQuant
             System.out.println("The Producer wants to have a current of amount of "+ idealStockQuantity +" "+ product.getProductName());
-            System.out.println( "Currently there are " + product.getStockQuantity() + " units left in stock");
             if( currentStockQuantity <= idealStockQuantity){
                 product.addStock(prod_id);
-                System.out.println("There are less " + product.getProductName()+ " than desired in stock, therefore we have added more in stock!");
-                //System.out.println("Now there is " + product.getStockQuantity() + " units left in stock"); // For some reason will actually update the last number but doesn't display the new quantity...
+                System.out.printf("+++!!!STOCKED ADDED producer wants to have a current of amount of %s %s 's\n" +
+                                "\t There is less than desired in stock." +
+                                "Therefore we have added more in stock!!!!!++++\n ",
+                                idealStockQuantity,product.getProductName());
             }
             else{
-                System.out.println("There are enough products in inventory right now!") ;
-
+                System.out.println("----!!!STOCK LEFT ALONE, Producer believes there are enough products of this item in inventory right now!!!----") ;
             }
-
-
         } catch (Exception e){
             System.out.println("***** PRODUCER has been interrupted while looking at Inventory*****");
-
         }
         System.out.println("***** PRODUCER has STOPPED looking at " + product.getProductName()+" in Inventory*****");
 
